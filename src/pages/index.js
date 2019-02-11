74972aa3fb272d387aa45cee58994e19aa9b5a45
Ben Halverson <benhalverson33@gmail.com>
2019-02-10 17:10:17 -0800

    reformatted code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,79 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
-import SEO from '../components/seo'
+import SEO from '../components/seo';
 import Videobackground from './videobackground';
 import './index.css';
 
 const IndexPage = () => (
   <Fragment>
-  <Videobackground/>
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      
-    <div className="row">
-      <div className="col">
-      <div className="jumbotron video sorta-transparent home">
-        <h1><strong className="text-uppercase">Strategize. Innovate. <br /> Go-to-market</strong></h1>
-        <h2>Turn Innovation into Revenue!</h2>
+    <Videobackground />
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
+      <div className="row">
+        <div className="col">
+          <div className="jumbotron video sorta-transparent home">
+            <h1>
+              <strong className="text-uppercase">
+                Strategize. Innovate. <br /> Go-to-market
+              </strong>
+            </h1>
+            <h2>Turn Innovation into Revenue!</h2>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
-    <div className="row bottom-row">
+      <div className="row bottom-row">
         <div className="col-md-3">
-          <div class="card" style={{width: '18rem;'}}>
+          <div class="card" style={{ width: '18rem;' }}>
             <div class="card-body">
               <h5 class="card-title">Corporates</h5>
-              < a href = "#"
-              class = "btn btn-primary" > Learn More </a>
+              <a href="#" class="btn btn-primary">
+                Learn More
+              </a>
               {/* <p class="card-text">Some quic    k example text to build on the card title and make up the bulk of the card's content.</p> */}
-              
             </div>
           </div>
         </div>
         <div className="col-md-3">
-          <div class="card" style={{width: '18rem;'}}>
+          <div class="card" style={{ width: '18rem;' }}>
             <div class="card-body">
               <h5 class="card-title">Startups</h5>
               {/* <p class="card-text">Some quic    k example text to build on the card title and make up the bulk of the card's content.</p> */}
-              <a href="#" class="btn btn-primary">Learn More</a>
+              <a href="#" class="btn btn-primary">
+                Learn More
+              </a>
             </div>
           </div>
         </div>
         <div className="col-md-3">
-          <div class="card" style={{width: '18rem;'}}>
+          <div class="card" style={{ width: '18rem;' }}>
             <div class="card-body">
-              <h5 class="card-title">Accelerators <br/> (private & govt.)</h5>
+              <h5 class="card-title">
+                Accelerators <br /> (private & govt.)
+              </h5>
               {/* <p class="card-text">Some quic    k example text to build on the card title and make up the bulk of the card's content.</p> */}
-              <a href="#" class="btn btn-primary">Learn More</a>
+              <a href="#" class="btn btn-primary">
+                Learn More
+              </a>
             </div>
           </div>
         </div>
         <div className="col-md-3">
-          <div class="card" style={{width: '18rem;'}}>
+          <div class="card" style={{ width: '18rem;' }}>
             <div class="card-body">
               <h5 class="card-title">Investors Family Offices & Angels</h5>
               {/* <p class="card-text">Some quic    k example text to build on the card title and make up the bulk of the card's content.</p> */}
-              <a href="#" class="btn btn-primary">Learn More</a>
+              <a href="#" class="btn btn-primary">
+                Learn More
+              </a>
             </div>
           </div>
         </div>
-
-    </div>
-  </Layout>
+      </div>
+    </Layout>
   </Fragment>
-)
+);
 
-export default IndexPage
+export default IndexPage;
