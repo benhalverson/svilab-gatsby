4fbb0db65726960091906d42817e034778d5bbdb
Ben Halverson <benhalverson33@gmail.com>
2019-03-15 21:23:06 -0700

    broken wip carousel

--- src/components/startupLogos.js
+++ src/components/startupLogos.js
@@ -1,15 +1,78 @@
 import React, { Component } from 'react';
+import { Carousel, CarouselItem } from 'reactstrap';
 
-import image1 from '../images/sv-logos.png';
-import image2 from '../images/sv-logos2.png';
+import SiliconValley from '../images/SiliconValley-temp.png';
+import Korea from '../images/Korea-temp.png';
+import China from '../images/China-temp.png';
+import Australia from '../images/Australia-temp.png';
+import Austria from '../images/Austria-temp.png';
+import Turkey from '../images/Turkey-temp.png';
 
 class StartupLogos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { moving: true };
-  }
+  state = { activeIndex: 0 };
+  onExiting = () => {
+    this.animating = true;
+  };
+
+  onExited = () => {
+    this.animating = false;
+  };
+
+  next = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+    console.log('index', nextIndex);
+    this.setState({ activeIndex: nextIndex });
+  };
+
+  previous = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  };
+
+  goToIndex = newIndex => {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  };
   render() {
-    return <div>Slides go here</div>;
+    const items = [
+      {
+        src: { SiliconValley }
+      },
+      {
+        src: { Korea }
+      },
+      {
+        src: { China }
+      },
+      {
+        src: { Australia }
+      },
+      {
+        src: { Austria }
+      },
+      {
+        src: { Turkey }
+      }
+    ];
+    const slides = items.map(item => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        />
+      );
+    });
+
+    return <Carousel activeIndex={activeIndex}>{slides}</Carousel>;
   }
 }
 
