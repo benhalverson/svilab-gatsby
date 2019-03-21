2d72241f30e52ca40c699cf860f0a2191f63747c
Ben Halverson <benhalverson33@gmail.com>
2019-03-20 23:11:20 -0700

    refactor(Accelerators cards): animanted card
    #9

---
+++ src/components/FlippingCard/AcceleratorsFrontCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AcceleratorsFront.css';
+
+const AcceleratorsFrontCard = () => {
+  return (
+    <div className="card-side side-front">
+      <div className="card corporate-color">
+        <div className="card-body">
+          <h5 className="card-title card-title--yellow card-title-spacing">
+            Accelerators <br /> (Private & Govt.)
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AcceleratorsFrontCard;
