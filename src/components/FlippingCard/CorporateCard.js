b5113c555748af257eace0f3ba5ac789d172bf3c
Ben Halverson <benhalverson33@gmail.com>
2019-03-22 16:21:32 -0700

    it works\!

--- src/components/FlippingCard/CorporateCard.js
+++ src/components/FlippingCard/CorporateCard.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import CorporateFrontCard from './CorporateFrontCard';
-import CorporateBackCard from './CorporateBackCard';
-import './Card.css';
+import '../../pages/flipper.css';
+
 class CorporateCard extends Component {
   render() {
     return (
-      <div className="card-container">
-        <div className="card-body">
-          <CorporateBackCard />
-
-          <CorporateFrontCard />
+      <div className="flipper-container">
+      <div className="front">
+        <div className="inner">
+          <p>front text</p>
         </div>
       </div>
+      <div className="back">
+        <div className="inner">
+          <div>
+            <h5 className="card-title card-title--yellow card-title-spacing">
+              Corporates
+              <br />
+              Learn How AI, Blockchain, IoT & more Helps your Business.
+            </h5>
+            <div className="white-text">
+              License, Acquire or Innovate & Go-to-Market
+            </div>
+            <a
+              href="/gotomarket"
+              className="btn btn-primary yellow-button"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
+import '../pages/flipper.css';
 import './index.css';
+
 import React, { Fragment } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-
-//TODO Finish work on making the cards flip
-// import FlipCard from 'react-flipcard';
-//TODO: refactor row and col to make responsive
 import Layout from '../components/layout';
 
 import SEO from '../components/seo';
@@ -12,22 +10,19 @@ import Videobackground from './videobackground';
 import InnovationPrograms from '../components/innovationPrograms';
 
 import StartupLogos from '../components/startupLogos';
-import CorporateCard from '../components/FlippingCard/CorporateCard';
-import StartupCard from '../components/FlippingCard/StartupCard';
-import AcceleratorCard from '../components/FlippingCard/AcceleratorsCard';
-import InvestorsCard from '../components/FlippingCard/InvestorsCard';
+
 const IndexPage = () => (
   <Fragment>
     <Container>
-      <Videobackground />
       <Layout>
         <SEO
           title="Home"
           keywords={[`innovation`, `go-to-market strategy `, `investors`]}
         />
+        <Videobackground />
 
-        <div className="row top-row">
-          <div className="col">
+        <Row className="top-row">
+          <Col md="12">
             <div className="jumbotron header video sorta-transparent home">
               <h1>
                 <strong className="text-uppercase">
@@ -36,31 +31,152 @@ const IndexPage = () => (
               </h1>
               <h2>Turn Innovation into Revenue!</h2>
             </div>
-          </div>
-        </div>
-        <div className="row bottom-row">
-          <div className="col-md-3">
-            <CorporateCard />
-          </div>
-
-          <div className="col-md-3">
-            <StartupCard />
-          </div>
-          <div className="col-md-3">
-            <AcceleratorCard />
-          </div>
-          <div className="col-md-3">
-            <InvestorsCard />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <InnovationPrograms />
-          </div>
-        </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="3">
+            <div className="flipper-container">
+              <div className="front">
+                <div className="inner">
+                  <h5 className="card-title card-title--yellow card-title-spacing">
+                    Corporates
+                    <br />
+                    Ready to Disrupt?
+                  </h5>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div>
+                    <h5 className="card-title card-title--yellow card-title-spacing">
+                      Corporates
+                      <br />
+                      Learn How AI, Blockchain, IoT & more Helps your Business.
+                    </h5>
+                    <div className="white-text">
+                      License, Acquire or Innovate & Go-to-Market
+                    </div>
+                    <a
+                      href="/gotomarket"
+                      className="btn btn-primary yellow-button"
+                    >
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md="3">
+            <div className="flipper-container">
+              <div className="front">
+                <div className="inner">
+                  <h5 className="card-title card-title--yellow card-title-spacing">
+                    Startups
+                    <br />
+                    Ready to Scale?
+                  </h5>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div>
+                    <h5 className="card-title card-title--yellow card-title-spacing">
+                      Startups
+                      <br />
+                      Grow Customers, Revenue & Valuation!
+                    </h5>
+                    <div className="white-text">
+                      Learn Go-to-Market, B2B Sales & Pitch-to-Investors. Get
+                      temp VP of Sales / COO
+                    </div>
+                    <a
+                      href="/gotomarket"
+                      className="btn btn-primary yellow-button"
+                    >
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md="3">
+            <div className="flipper-container">
+              <div className="front">
+                <div className="inner">
+                  <h5 className="card-title card-title--yellow card-title-spacing">
+                    Accelerators, (Private & Govt.)
+                    <br />
+                    Need Programs for Customer & Revenue Growth?
+                  </h5>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div>
+                    <h5 className="card-title card-title--yellow card-title-spacing">
+                      Accelerators, (Private & Govt.)
+                      <br />
+                      Increase Startup Revenue!
+                    </h5>
+                    <div className="white-text">
+                      Learn B2B Sales, Go-to-Market & How to Pitch to Investors
+                    </div>
+                    <a
+                      href="/gotomarket"
+                      className="btn btn-primary yellow-button"
+                    >
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md="3">
+            <div className="flipper-container">
+              <div className="front">
+                <div className="inner">
+                  <h5 className="card-title card-title--yellow card-title-spacing">
+                    Investors, Family Offices & Angels
+                    <br />
+                    Investing in Tech? Are Startup Revenues too Low?
+                  </h5>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div>
+                    <h5 className="card-title card-title--yellow card-title-spacing">
+                      Investors, Family Offices & Angels
+                      <br />
+                      Increase Startup Revenue & Valuation
+                    </h5>
+                    <div className="white-text">
+                      Learn Investing in Startups, or, Get Sales / Operations
+                      Help for your Startups
+                    </div>
+                    <a
+                      href="/gotomarket"
+                      className="btn btn-primary yellow-button"
+                    >
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
         <Row>
           <Col>
-            <div className="moveToBottom">
+            <InnovationPrograms />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
               <StartupLogos />
             </div>
           </Col>
