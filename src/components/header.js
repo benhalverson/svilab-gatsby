8b5a2a6b0d51587b04795cf3cad912d515d40c6b
Ben Halverson <benhalverson33@gmail.com>
2019-03-24 20:54:30 -0700

    fix(navbar): the logo icon will not go back to the homepage from any other page

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="./">
+    <a className="navbar-brand" href="/">
       sv-ilab
     </a>
     <button
