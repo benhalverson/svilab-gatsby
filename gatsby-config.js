d2d083ea66200574406bf5ac1e1f263754e86020
Ben Halverson <benhalverson33@gmail.com>
2019-03-27 23:41:04 -0700

    the real fix after testing locally

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
         exclude: ['/beta/'],
         cookieDomain: 'svilab.com'
       }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
