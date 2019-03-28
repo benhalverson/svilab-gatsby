cf53511c7e3a207cf0aadf37f01d6914f4fd323e
Ben Halverson <benhalverson33@gmail.com>
2019-03-27 22:18:09 -0700

    comment out demo video

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import demo from '../videos/demo.mp4'
+import demo from '../videos/demo.mp4';
 import { Jumbotron } from 'reactstrap';
 const VideoPlayer = () => {
-
   // const videoSource = `https://www.youtube.com/watch?v=Bey4XXJAqS8`;
 
-  return(
-    
+  return (
     <Jumbotron className="">
-    <iframe src={demo} title="video player" />
+      {/* <iframe src={demo} title="video player" /> */}
+      {/* TODO: need video from steve */}
     </Jumbotron>
-      
-    
-  )
-}
+  );
+};
 export default VideoPlayer;
